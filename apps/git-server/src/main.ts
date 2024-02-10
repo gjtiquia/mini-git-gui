d0a9bbca2a2d8028c1ecd85b73e9f95212dd7192
GJ Tiquia <gershom.tiquia@gmail.com>
2024-02-10 23:37:12 +0800

    refactor: renamed index.ts to main.ts

--- apps/git-server/src/main.ts
+++ apps/git-server/src/main.ts
@@ -3,7 +3,8 @@ import { getAllCommitsAsync } from "./getAllCommitsAsync";
 main()
 
 async function main() {
-    const rootDirectory = "/Users/gjtiquia/Documents/Projects/SelfProjects/mini-text-editor";
+    // const rootDirectory = "/Users/gjtiquia/Documents/Projects/SelfProjects/mini-text-editor";
+    const rootDirectory = "/Users/EuniceChen/Downloads/_GJDocuments/mini-git-gui"
     const commits = await getAllCommitsAsync(rootDirectory);
 
     console.log(commits);
