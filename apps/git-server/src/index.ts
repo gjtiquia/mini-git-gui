76373a5f6597ce01b6ee486a4743d0581ad9b4c8
GJ Tiquia <gershom.tiquia@gmail.com>
2024-02-10 16:39:37 +0800

    WIP: read git log with formatting

--- apps/git-server/src/index.ts
+++ apps/git-server/src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello World!")
+import { spawn } from "child_process";
+
+// Reference: 
+// - https://git-scm.com/docs/git-log#_commit_formatting
+// - https://git-scm.com/docs/git-log#_pretty_formats
+
+// --all: show branches as well
+// 
+const child = spawn("git", ["log", "--all", "--pretty=format:Subject: %s%nAuthor: %an%nHash: %H%nParent Hashes: %P%nAuthor Date: %ad%nUNIX Timestamp: %at%n"], {
+    cwd: "/Users/gjtiquia/Documents/Projects/SelfProjects/mini-text-editor"
+});
+
+child.stdout.on('data', (data) => {
+    console.log(`child stdout:\n${data}`);
+});
+
+child.stderr.on('data', (data) => {
+    console.error(`child stderr:\n${data}`);
+});
+
+child.on('exit', function (code, signal) {
+    console.log('child process exited with ' +
+        `code ${code} and signal ${signal}`);
+});
