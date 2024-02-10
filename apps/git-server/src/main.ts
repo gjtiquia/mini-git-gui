c5ef36cbc6e7877f2049db77961299506d33ea09
GJ Tiquia <gershom.tiquia@gmail.com>
2024-02-10 23:42:37 +0800

    chore: setup express

--- apps/git-server/src/main.ts
+++ apps/git-server/src/main.ts
@@ -1,12 +1,29 @@
+import express from "express"
+import cors from "cors";
 import { getAllCommitsAsync } from "./getAllCommitsAsync";
 
-main()
+console.log("Running @mini-git-gui/git-server...")
 
-async function main() {
+const app = express();
+const PORT = 3000;
+
+// Global Middleware
+app.use(cors()); // Enable all origins
+
+// Set Routes
+app.get("/", async (req, res) => {
+
     // const rootDirectory = "/Users/gjtiquia/Documents/Projects/SelfProjects/mini-text-editor";
     const rootDirectory = "/Users/EuniceChen/Downloads/_GJDocuments/mini-git-gui"
     const commits = await getAllCommitsAsync(rootDirectory);
 
     console.log(commits);
     console.log("total:", commits.length);
-}
+
+    res.send(commits);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at: http://localhost:${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+});
