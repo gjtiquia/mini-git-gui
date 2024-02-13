a3c564bdb16ced2f6babd88c374d3f9360c1e8f5
GJ Tiquia <gershom.tiquia@gmail.com>
2024-02-13 14:55:58 +0800

    chore: remove git server console logs

--- apps/git-server/src/routes/appRouter.ts
+++ apps/git-server/src/routes/appRouter.ts
@@ -5,13 +5,11 @@ import { getAllCommitsAsync } from '../getAllCommitsAsync';
 const trpcAppRouter = router({
     getAllCommits: publicProcedure
         .query(async () => {
+
             // const rootDirectory = "/Users/gjtiquia/Documents/Projects/SelfProjects/mini-text-editor";
-            const rootDirectory = "/Users/EuniceChen/Downloads/_GJDocuments/mini-git-gui"
+            const rootDirectory = "/Users/EuniceChen/Downloads/_GJDocuments/headless-game-engine"
+
             const commits = await getAllCommitsAsync(rootDirectory);
-
-            console.log(commits);
-            console.log("total:", commits.length);
-
             return commits;
         })
 });
