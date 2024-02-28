3ef8599c0a4de197a990da16e48e4a720ede1b29
GJ Tiquia <gershom.tiquia@gmail.com>
2024-02-28 13:18:51 +0800

    feat: fetch dialog

--- apps/web-gui/src/components/FetchButton.tsx
+++ apps/web-gui/src/components/FetchButton.tsx
@@ -1,13 +1,35 @@
+import {
+    AlertDialog,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 import { ArrowDown } from "lucide-react";
 import { Button } from "./ui/button";
 import { useQueryClient } from "@tanstack/react-query";
 import { trpc } from "@/lib/trpc";
+import { useState } from "react";
 
 
 export function FetchButton() {
+    return (
+        <FetchDialog />
+    );
+}
+
+function FetchDialog() {
+
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
 
     const queryClient = useQueryClient();
     const fetchMutation = trpc.fetch.useMutation({
+        onSuccess: () => {
+            setIsDialogOpen(false);
+        },
         onSettled: () => {
             // use queryClient instead of trpc utils because not all queries are via trpc
             queryClient.invalidateQueries();
@@ -19,8 +41,48 @@ export function FetchButton() {
     }
 
     return (
-        <Button size="icon" variant={"secondary"} onClick={() => fetch()}>
-            <ArrowDown className="h-[1.2rem] w-[1.2rem]" />
-        </Button>
-    );
+        <AlertDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+            <AlertDialogTrigger asChild>
+                <Button size="icon" variant={"secondary"}>
+                    <ArrowDown className="h-[1.2rem] w-[1.2rem]" />
+                </Button>
+            </AlertDialogTrigger>
+
+            <AlertDialogContent>
+
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Fetch</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        Fetch latest changes from remote repository.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+
+                {fetchMutation.isError
+                    && <p className="text-center text-red-500">{fetchMutation.error.message}</p>
+                }
+
+                {(fetchMutation.isPending)
+                    ? <PendingFetchFooter />
+                    : <DefaultFetchFooter onFetchClicked={() => fetch()} />
+                }
+            </AlertDialogContent>
+        </AlertDialog>
+    )
+}
+
+function DefaultFetchFooter(props: { onFetchClicked: () => void }) {
+    return (
+        <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <Button onClick={props.onFetchClicked}>Fetch</Button>
+        </AlertDialogFooter>
+    )
+}
+
+function PendingFetchFooter() {
+    return (
+        <AlertDialogFooter>
+            <Button disabled>Fetching...</Button>
+        </AlertDialogFooter>
+    )
 }
