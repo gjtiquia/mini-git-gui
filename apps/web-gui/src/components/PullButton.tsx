8475eb3f305d0eda1329deeacc71f89cd71867d6
GJ Tiquia <gershom.tiquia@gmail.com>
2024-02-28 13:24:29 +0800

    feat: pull dialog

--- apps/web-gui/src/components/PullButton.tsx
+++ apps/web-gui/src/components/PullButton.tsx
@@ -1,13 +1,34 @@
+import {
+    AlertDialog,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 import { ArrowDownToLine } from "lucide-react";
 import { Button } from "./ui/button";
 import { useQueryClient } from "@tanstack/react-query";
 import { trpc } from "@/lib/trpc";
-
+import { useState } from "react";
 
 export function PullButton() {
+    return (
+        <PullDialog />
+    );
+}
+
+function PullDialog() {
+
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
 
     const queryClient = useQueryClient();
     const pullMutation = trpc.pull.useMutation({
+        onSuccess: () => {
+            setIsDialogOpen(false);
+        },
         onSettled: () => {
             // use queryClient instead of trpc utils because not all queries are via trpc
             queryClient.invalidateQueries();
@@ -19,8 +40,48 @@ export function PullButton() {
     }
 
     return (
-        <Button size="icon" variant={"secondary"} onClick={() => pull()}>
-            <ArrowDownToLine className="h-[1.2rem] w-[1.2rem]" />
-        </Button>
-    );
+        <AlertDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+            <AlertDialogTrigger asChild>
+                <Button size="icon" variant={"secondary"}>
+                    <ArrowDownToLine className="h-[1.2rem] w-[1.2rem]" />
+                </Button>
+            </AlertDialogTrigger>
+
+            <AlertDialogContent>
+
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Pull</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        Pull remote branches and merge them to your local branch.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+
+                {pullMutation.isError
+                    && <p className="text-center text-red-500">Error: {pullMutation.error.message}</p>
+                }
+
+                {(pullMutation.isPending)
+                    ? <PendingPullFooter />
+                    : <DefaultPullFooter onPullClicked={() => pull()} />
+                }
+            </AlertDialogContent>
+        </AlertDialog>
+    )
+}
+
+function DefaultPullFooter(props: { onPullClicked: () => void }) {
+    return (
+        <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <Button onClick={props.onPullClicked}>Pull</Button>
+        </AlertDialogFooter>
+    )
+}
+
+function PendingPullFooter() {
+    return (
+        <AlertDialogFooter>
+            <Button disabled>Pulling...</Button>
+        </AlertDialogFooter>
+    )
 }
