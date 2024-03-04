2bbefdeb7ca861ff88c429ce6b06c079dbdf9c4c
GJ Tiquia <gershom.tiquia@gmail.com>
2024-03-04 15:36:29 +0800

    chore: package setup

--- apps/web-gui/src/lib/trpc.ts
+++ apps/web-gui/src/lib/trpc.ts
@@ -1,6 +1,6 @@
 import { createTRPCReact } from '@trpc/react-query';
 import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
-import type { AppRouter } from "@/../../git-server";
+import type { AppRouter } from "@/../../git-server/src/index";
 
 export const trpc = createTRPCReact<AppRouter>();
 export type AppRouterInput = inferRouterInputs<AppRouter>;
